0be171730786fa9bb152ad50e3396d125d6244e6
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-28 16:23:57 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,12 @@ export const metadata: Metadata = {
 }
 
 const Page: React.FC = () => {
-  return (
-    <div>
-      <h1 className={styles.home}>Hello, Next.js!</h1>
-      <Link href="/callback">
+  return ( 
+    <div className={styles.nav}>
+    <h1 className={styles.home}>Chronic</h1>
+    <Link href="/callback">
         <a>Callback page</a>
-      </Link>
-    </div>
+    </Link>
   );
 };
 
