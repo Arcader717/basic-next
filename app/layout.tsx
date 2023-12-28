0d485b184f5a8ac703cd0f901dc91fb6dad59d31
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-28 15:49:39 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en">
-        <body>{children}</body>
+        <body class="bg-white">{children}</body>
       </html>
     )
   }
