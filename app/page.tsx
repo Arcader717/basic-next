b0a5145e14c2632201ef883c24841a264ccc84f8
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-28 15:31:13 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-'use client'
-
 import Link from 'next/link';
 import './global.css';
 
 const Page: React.FC = () => {
   return (
     <div>
-      <h1>Hello, Next.js!</h1>
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
       <Link href="/callback">
         <a>Callback page</a>
       </Link>
