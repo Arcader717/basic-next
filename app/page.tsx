7ea2074ece8407ace21339cb57afe322ae2aa1d8
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-28 15:01:59 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@
 import Link from 'next/link'
 import './global.css'
 
-export default function Page() {
+const Page() {
   return (
     <h1>Hello, Next.js!</h1>
     <Link href="/callback">Check out the callback</Link>
   )
 }
+
+export default Page;
