a4e9a134a5d916792267918c91af1804c3ab38df
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-29 17:44:45 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './global.css';
 import styles from './index.module.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
     children,
@@ -12,6 +13,7 @@ export default function RootLayout({
         <body className={styles.body}>
             {children}
             <Analytics/>
+            <SpeedInsights/>
         </body>
       </html>
     )
