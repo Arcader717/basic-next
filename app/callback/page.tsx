86aa134d5543444cc66663b53d6d1381cee39e67
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-28 18:42:19 -0600

    Update page.tsx

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -1,14 +1,24 @@
-import Link from 'next/link'
+'use client'
+
+import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
 
 const Page = () => {
+  const p = useSearchParams();
+  if (p.has("code")) {
+    const code = p.get("code")
+  } else {
+    const code = p.get("Nothing :(")
+  }
   return (
     <div>
       <h1>This is the callback page</h1>
-      <Link href="/">
-        <a>Go home!</a>
-      </Link>
+      <p>Currently used to get the search params</p>
+      <br/><br/><br/>
+      <p>Add '?code=' and then whatever you want then reload to get it to show below</p>
+      <br/><br/><br/>
+      <h3>{code}</h3>
     </div>
-  )
-}
+
 
 export default Page;
