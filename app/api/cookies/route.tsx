0a003ed20dd003c92d3e3dbc0bcdc8eae2e50122
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-29 18:42:36 -0600

    Update route.tsx

--- app/api/cookies/route.tsx
+++ app/api/cookies/route.tsx
@@ -1,7 +1,12 @@
+'use server'
+
+import { cookies } from 'next/headers'
 import { headers } from 'next/headers'
 
 export const runtime = "edge"
 
-export async function GET() {
-  
+export async function POST() {
+  const heads = headers()
+  cookies().set(heads.get('name'), heads.get('value'))
+  cookies().set("ip", heads.get("x-real-ip"))
 }
