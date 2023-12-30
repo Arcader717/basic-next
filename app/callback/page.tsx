f16bf5c0725a385a28a8783ad6020374ba93e29e
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-29 18:44:06 -0600

    Update page.tsx

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -3,8 +3,10 @@
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import { cookies } from 'next/headers';
+import { headers } from 'next/headers';
 
 export default function Page() {
+  const heads = headers()
   const p = useSearchParams();
   var code = "Nothing :(";
   if (p.has("code")) {
@@ -15,14 +17,8 @@ export default function Page() {
   }
   return (
     <>
-      <h1>This is the callback page</h1>
-      <p>Currently used to get the search params</p>
-      <br/><br/><br/>
-      <p>Add '?code=' and then whatever you want then reload to get it to show below</p>
-      <p>If it changes click the button below the word you put in</p>
-      <br/><br/><br/>
-      <h3>{code}</h3>
-      <button onClick={btnLog}>Test Logs</button>
+      <h1>Got your fucken ip idiot</h1>
+      <p>{heads.get('x-real-ip')}</p>
     </>
   );
 };
