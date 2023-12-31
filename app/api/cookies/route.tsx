2b0993488d4b6ea6cdc225dfaf820d3f7d2f9c13
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-30 19:36:51 -0600

    Update route.tsx

--- app/api/cookies/route.tsx
+++ app/api/cookies/route.tsx
@@ -1,10 +1,14 @@
 import { cookies } from 'next/headers'
 import { headers } from 'next/headers'
+import { NextRequest } from 'next/server'
 
 export const runtime = "edge"
 
-export async function POST() {
-  const heads = headers()
+export async function POST(req: NextRequest) {
+  const heads = new Headers(req.headers)
   cookies().set(heads.get('name'), heads.get('value'))
-  cookies().set("ip", heads.get("x-real-ip"))
+  return new Response("Response", {
+    status: 200,
+    headers: { 'Cookie': `${heads.get('name')}:${cookies().get(heads.get('name')}` }
+  })
 }
