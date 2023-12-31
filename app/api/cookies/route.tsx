6c6acac2160007b43ca57e8a72e345e96c6e9f14
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-30 20:44:45 -0600

    Update route.tsx

--- app/api/cookies/route.tsx
+++ app/api/cookies/route.tsx
@@ -1,7 +1,21 @@
 'use server'
 
+import { cookies } from 'next/headers'
+import { headers } from 'next/headers'
+
 export const runtime = "edge"
 
 export async function GET(r: Request) {
-  return //cookie get return
+  const c = cookies()
+  const h = headers()
+  const name = h.get('name')
+  return c.get(name)
+}
+
+export async function POST(r: Request) {
+  const c = cookies()
+  const h = headers()
+  const name = h.get('name')
+  const value = h.get('value')
+  c.set(name, value)
 }
