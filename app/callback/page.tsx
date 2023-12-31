f0156cc2cb6017e972d3d8971fafd7035e73d078
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-30 20:02:47 -0600

    Update page.tsx

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -1,36 +1,17 @@
 'use client'
 
 import Link from 'next/link';
-import { headers } from 'next/headers';
 import { useSearchParams } from 'next/navigation';
-import { cookies } from 'next/headers';
+import { set, get } from '../components/cookies';
 
 export default async function Page() {
-  const heads = headers()
-  const cook = cookies()
-  const params = useSearchParams()
+  const p = useSearchParams()
   const code = params.get('code')
-  await fetch("https://www.chronicbot.xyz/api/cookies", {
-    method: "POST",
-    headers: {
-      "name": "code",
-      "value": code
-    }
-  })
-  function btnLog() {
-    console.log("Vercel log test");
-  }
+  set('code', code)
   const newCode = cook.get('code')
   return (
     <>
-      <h1>Got your fucken IP idiot</h1>
-      <h2>{heads.get('x-real-ip')}</h2>
-      <br/><br/><br/>
-      <h3>Consider yourself lucky</h3>
-      <p>Just by opening a website, your ip can be displayed, and used to the website's advantage. So be careful about who you trust</p>
-      <br/><br/><br/>
-      <p>{newCode}</p>
-      <h5>Don't tell the others, this is important</h5>
+      <p>code is {get('code')}</p>
     </>
   );
 };
