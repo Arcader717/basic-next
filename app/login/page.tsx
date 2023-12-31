67c2f90095b78b63f999f808a9f78bbd53c35204
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-30 22:31:48 -0600

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 import { permanentRedirect } from 'next/navigation'
 
 export default function Page() {
-    return permanentRedirect('/callback')
+    return permanentRedirect('/callback?code=:)')
 }
