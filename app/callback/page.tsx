f12f21e403550224e12baa668f10ce65ea6f39e3
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-30 20:53:10 -0600

    Update page.tsx

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -2,16 +2,16 @@
 
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-import { set, get } from '../components/cookies';
+import { setCookies, getCookies } from '../components/cookies';
 
 export default async function Page() {
   const p = useSearchParams()
   const code = params.get('code')
-  set('code', code)
+  setCookies('code', code)
   const newCode = cook.get('code')
   return (
     <>
-      <p>code is {get('code')}</p>
+      <p>code is {getCookies('code')}</p>
     </>
   );
 };
