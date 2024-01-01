c3bfaad8f4b7fe5cf0c2953a12adfc1c270f868e
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-31 18:20:38 -0600

    Update page.tsx

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -2,11 +2,17 @@
 
 import { useSearchParams } from 'next/navigation';
 import { setCookie, getCookie } from '../components/cookies';
-import { permanentRedirect } from 'next/navigation'
+import { permanentRedirect } from 'next/navigation';
+import { Link } from 'next/navigation';
 
 export default async function Page() {
   const p = useSearchParams()
   const code = p.get('code')
   setCookie('code', code)
-  return permanentRedirect("/dash")
+  return (
+    <>
+      <p>Got code</p>
+      <
+    </>
+  )
 };
