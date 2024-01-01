f81d3637fa4ffbb6862433556926502d0ce53029
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-31 18:50:25 -0600

    Update page.tsx

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation';
-import { setCookie, getCookie } from '../components/cookies';
-import Link from 'next/link';
+// import { setCookie, getCookie } from '../components/cookies';
+// import Link from 'next/link';
 
 export default async function Page() {
-  const p = useSearchParams()
   return (
     <>
       <p>Hi</p>
