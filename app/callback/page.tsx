545be2c7dde41213e9b09b84340b79f80e56408e
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-31 19:13:35 -0600

    Update page.tsx

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -1,18 +1,15 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 // import { setCookie, getCookie } from '../components/cookies';
 // import Link from 'next/link';
 
 export default async function Page() {
-  let params = new URLSearchParams(document.location.search);
-  let code = "Nothing :("
-  if (params.has("code")) {
-    let code = params.get("code")
-  };
+  let router = useRouter();
+  const { keyword } = router.query;
   return (
     <>
-      <p>Hi {code}</p>
+      <p>Hi {keyword}</p>
     </>
   )
 };
