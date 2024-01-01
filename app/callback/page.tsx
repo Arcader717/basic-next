fc113c964caf24eae4556da94b945e864a87b550
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-31 18:57:55 -0600

    Update page.tsx

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation'
 // import { setCookie, getCookie } from '../components/cookies';
 // import Link from 'next/link';
 
 export default async function Page() {
-  let p = useSearchParams()
+  const searchParams = useSearchParams()
+  if (searchParams.has("code")) {
+    const code = searchParams.get("code")
+  } else {
+    const code = "Nothing :("
+  }
   return (
     <>
-      <p>Hi</p>
+      <p>Hi {code}</p>
     </>
   )
 };
