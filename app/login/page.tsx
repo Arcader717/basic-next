7c4c62726c5b8f3c7d0e7087dc7cab1cc33e7da2
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-31 18:02:00 -0600

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,12 @@ import { permanentRedirect } from 'next/navigation'
 import { setCookie, getCookie } from '../components/cookies';
 
 export default function Page() {
-    setCookie("code", ":)")
-    return permanentRedirect('/callback')
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let randomString = '';
+    let length = 16;
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomString += characters.charAt(randomIndex);
+    }
+    return permanentRedirect(`https://discord.com/oauth2/authorize?response_type=code&client_id=1163179429640544267&scope=identify&redirect_uri=https%3A%2F%2Fchronicbot.xyz%2Fcallback&prompt=consent&state=${randomString}`)
 }
