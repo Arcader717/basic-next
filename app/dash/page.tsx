cc6a4bd2b9cb350059ea864ffb16ecbdd8320c6c
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-31 18:02:57 -0600

    Update page.tsx

--- app/dash/page.tsx
+++ app/dash/page.tsx
@@ -2,9 +2,7 @@ import { permanentRedirect } from 'next/navigation'
 import { getCookie, setCookie } from '../components/cookies'
 
 export default function Page() {
-  const code = getCookie('code') 
   return (
-    <p>Testing page for sessions and cookies</p>
-    <p>Your code is {code}</p>
+    <p>Testing page</p>
   )
 }
